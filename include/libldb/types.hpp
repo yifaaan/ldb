6077ceb18609ae3b66c24ce976e8db652cf8dd8d
yifaaan <0219lyf@gmail.com>
2024-12-17 19:00:27 +0800

    Add virtual address and format code

--- include/libldb/types.hpp
+++ include/libldb/types.hpp
@@ -3,10 +3,50 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 
-namespace ldb {
-using byte64 = std::array<std::byte, 8>;
-using byte128 = std::array<std::byte, 16>;
-}  // namespace ldb
+namespace ldb
+{
+    using byte64 = std::array<std::byte, 8>;
+    using byte128 = std::array<std::byte, 16>;
+
+    /// Representing virtual addresses
+    class virt_addr
+    {
+    public:
+        virt_addr() = default;
+        explicit virt_addr(std::uint64_t addr) : addr_(addr)
+        {
+        }
+
+        std::uint64_t addr() const
+        {
+            return addr_;
+        }
+
+        virt_addr operator+(std::uint64_t offset) const
+        {
+            return virt_addr(addr_ + offset);
+        }
+        virt_addr operator-(std::uint64_t offset) const
+        {
+            return virt_addr(addr_ - offset);
+        }
+        virt_addr& operator+=(std::uint64_t offset)
+        {
+            addr_ += offset;
+            return *this;
+        }
+        virt_addr& operator-=(std::uint64_t offset)
+        {
+            addr_ -= offset;
+            return *this;
+        }
+        bool operator<=>(const virt_addr& other) const = default;
+
+    private:
+        std::uint64_t addr_ = 0;
+    };
+} // namespace ldb
 
 #endif
--- test/tests.cpp
+++ test/tests.cpp
@@ -5,143 +5,145 @@
 #include <libldb/error.hpp>
 #include <libldb/pipe.hpp>
 #include <libldb/process.hpp>
-
-#include "libldb/register_info.hpp"
-#include "libldb/types.hpp"
+#include <libldb/register_info.hpp>
+#include <libldb/types.hpp>
 
 using namespace ldb;
 
-namespace {
-/// Check if a process exists.
-bool process_exists(pid_t pid) {
-  auto ret = kill(pid, 0);
-  return ret != -1 && errno != ESRCH;
+namespace
+{
+    /// Check if a process exists.
+    bool process_exists(pid_t pid)
+    {
+        auto ret = kill(pid, 0);
+        return ret != -1 && errno != ESRCH;
+    }
+
+    char get_process_status(pid_t pid)
+    {
+        auto stat_path = std::string("/proc/") + std::to_string(pid) + "/stat";
+        std::fstream stat{stat_path};
+        std::string data;
+        std::getline(stat, data);
+        auto index_of_last_parenthesis = data.rfind(')');
+        auto index_of_status_indicator = index_of_last_parenthesis + 2;
+        return data[index_of_status_indicator];
+    }
+} // namespace
+
+TEST_CASE("process::launch success", "[process]")
+{
+    auto proc = process::launch("yes");
+    REQUIRE(process_exists(proc->pid()));
 }
 
-char get_process_status(pid_t pid) {
-  auto stat_path = std::string("/proc/") + std::to_string(pid) + "/stat";
-  std::fstream stat{stat_path};
-  std::string data;
-  std::getline(stat, data);
-  auto index_of_last_parenthesis = data.rfind(')');
-  auto index_of_status_indicator = index_of_last_parenthesis + 2;
-  return data[index_of_status_indicator];
-}
-}  // namespace
-
-TEST_CASE("process::launch success", "[process]") {
-  auto proc = process::launch("yes");
-  REQUIRE(process_exists(proc->pid()));
+TEST_CASE("process::launch no such program", "[process]")
+{
+    REQUIRE_THROWS_AS(process::launch("you_do_not_have_to_be_good"), error);
 }
 
-TEST_CASE("process::launch no such program", "[process]") {
-  REQUIRE_THROWS_AS(process::launch("you_do_not_have_to_be_good"), error);
-}
-
-TEST_CASE("process::attach success", "[process]") {
-  auto target = process::launch(
-      "/root/workspace/ldb/build/test/targets/run_endlessly", false);
-  auto proc = process::attach(target->pid());
-  REQUIRE(get_process_status(target->pid()) == 't');
-}
-
-TEST_CASE("process::attach invalid PID", "[process]") {
-  REQUIRE_THROWS_AS(process::attach(0), error);
-}
-
-TEST_CASE("process::resume success", "[process]") {
-  {
-    auto proc =
-        process::launch("/root/workspace/ldb/build/test/targets/run_endlessly");
-    proc->resume();
-    auto status = get_process_status(proc->pid());
-    auto success = status == 'R' or status == 'S';
-    REQUIRE(success);
-  }
-  {
-    auto target =
-        process::launch("/root/workspace/ldb/build/test/targets/run_endlessly");
+TEST_CASE("process::attach success", "[process]")
+{
+    auto target = process::launch("/root/workspace/ldb/build/test/targets/run_endlessly", false);
     auto proc = process::attach(target->pid());
+    REQUIRE(get_process_status(target->pid()) == 't');
+}
+
+TEST_CASE("process::attach invalid PID", "[process]")
+{
+    REQUIRE_THROWS_AS(process::attach(0), error);
+}
+
+TEST_CASE("process::resume success", "[process]")
+{
+    {
+        auto proc = process::launch("/root/workspace/ldb/build/test/targets/run_endlessly");
+        proc->resume();
+        auto status = get_process_status(proc->pid());
+        auto success = status == 'R' or status == 'S';
+        REQUIRE(success);
+    }
+    {
+        auto target = process::launch("/root/workspace/ldb/build/test/targets/run_endlessly");
+        auto proc = process::attach(target->pid());
+        proc->resume();
+        auto status = get_process_status(proc->pid());
+        auto success = status == 'R' or status == 'S';
+        REQUIRE(!success);
+        // INFO("msg");
+    }
+}
+
+TEST_CASE("process::resume already terminated", "[process]")
+{
+    auto proc = process::launch("/root/workspace/ldb/build/test/targets/end_immediately");
+    INFO(proc->pid());
     proc->resume();
-    auto status = get_process_status(proc->pid());
-    auto success = status == 'R' or status == 'S';
-    REQUIRE(!success);
-    // INFO("msg");
-  }
+    proc->wait_on_signal();
+    REQUIRE_THROWS_AS(proc->resume(), error);
 }
 
-TEST_CASE("process::resume already terminated", "[process]") {
-  auto proc =
-      process::launch("/root/workspace/ldb/build/test/targets/end_immediately");
-  INFO(proc->pid());
-  proc->resume();
-  proc->wait_on_signal();
-  REQUIRE_THROWS_AS(proc->resume(), error);
+TEST_CASE("Write register works", "[register]")
+{
+    bool close_on_exec = false;
+    ldb::pipe channel(close_on_exec);
+
+    auto proc = process::launch("/root/workspace/ldb/build/test/targets/reg_write", true, channel.get_write());
+    channel.close_write();
+
+    proc->resume();
+    proc->wait_on_signal();
+
+    // Write GPR
+    auto& regs = proc->get_registers();
+    regs.write_by_id(register_id::rsi, 0xabcdabcd);
+    proc->resume();
+    proc->wait_on_signal();
+    auto output = channel.read();
+    REQUIRE(to_string_view(output) == "0xabcdabcd");
+    // Write MMX
+    regs.write_by_id(register_id::mm0, 0xba5eba11);
+    proc->resume();
+    proc->wait_on_signal();
+    output = channel.read();
+    REQUIRE(to_string_view(output) == "0xba5eba11");
+    // Write SSE's XMM
+    regs.write_by_id(register_id::xmm0, 42.24);
+    proc->resume();
+    proc->wait_on_signal();
+    output = channel.read();
+    REQUIRE(to_string_view(output) == "42.24");
+    /// x87
+    regs.write_by_id(register_id::st0, 42.24l);
+    regs.write_by_id(register_id::fsw, std::uint16_t{0b0011100000000000});
+    regs.write_by_id(register_id::ftw, std::uint16_t{0b0011111111111111});
+    proc->resume();
+    proc->wait_on_signal();
+    output = channel.read();
+    REQUIRE(to_string_view(output) == "42.24");
 }
 
-TEST_CASE("Write register works", "[register]") {
-  bool close_on_exec = false;
-  ldb::pipe channel(close_on_exec);
+TEST_CASE("Read register works", "[register]")
+{
+    auto proc = process::launch("/root/workspace/ldb/build/test/targets/reg_read");
+    auto& regs = proc->get_registers();
+    proc->resume();
+    proc->wait_on_signal();
+    REQUIRE(regs.read_by_id_as<std::uint64_t>(register_id::r13) == 0xcafecafe);
 
-  auto proc =
-      process::launch("/root/workspace/ldb/build/test/targets/reg_write", true,
-                      channel.get_write());
-  channel.close_write();
+    proc->resume();
+    proc->wait_on_signal();
+    REQUIRE(regs.read_by_id_as<std::uint8_t>(register_id::r13b) == 42);
 
-  proc->resume();
-  proc->wait_on_signal();
+    proc->resume();
+    proc->wait_on_signal();
+    REQUIRE(regs.read_by_id_as<byte64>(register_id::mm0) == to_byte64(0xba5eba11ull));
 
-  // Write GPR
-  auto& regs = proc->get_registers();
-  regs.write_by_id(register_id::rsi, 0xabcdabcd);
-  proc->resume();
-  proc->wait_on_signal();
-  auto output = channel.read();
-  REQUIRE(to_string_view(output) == "0xabcdabcd");
-  // Write MMX
-  regs.write_by_id(register_id::mm0, 0xba5eba11);
-  proc->resume();
-  proc->wait_on_signal();
-  output = channel.read();
-  REQUIRE(to_string_view(output) == "0xba5eba11");
-  // Write SSE's XMM
-  regs.write_by_id(register_id::xmm0, 42.24);
-  proc->resume();
-  proc->wait_on_signal();
-  output = channel.read();
-  REQUIRE(to_string_view(output) == "42.24");
-  /// x87
-  regs.write_by_id(register_id::st0, 42.24l);
-  regs.write_by_id(register_id::fsw, std::uint16_t{0b0011100000000000});
-  regs.write_by_id(register_id::ftw, std::uint16_t{0b0011111111111111});
-  proc->resume();
-  proc->wait_on_signal();
-  output = channel.read();
-  REQUIRE(to_string_view(output) == "42.24");
-}
+    proc->resume();
+    proc->wait_on_signal();
+    REQUIRE(regs.read_by_id_as<byte128>(register_id::xmm0) == to_byte128(64.125));
 
-TEST_CASE("Read register works", "[register]") {
-  auto proc =
-      process::launch("/root/workspace/ldb/build/test/targets/reg_read");
-  auto& regs = proc->get_registers();
-  proc->resume();
-  proc->wait_on_signal();
-  REQUIRE(regs.read_by_id_as<std::uint64_t>(register_id::r13) == 0xcafecafe);
-
-  proc->resume();
-  proc->wait_on_signal();
-  REQUIRE(regs.read_by_id_as<std::uint8_t>(register_id::r13b) == 42);
-
-  proc->resume();
-  proc->wait_on_signal();
-  REQUIRE(regs.read_by_id_as<byte64>(register_id::mm0) ==
-          to_byte64(0xba5eba11ull));
-
-  proc->resume();
-  proc->wait_on_signal();
-  REQUIRE(regs.read_by_id_as<byte128>(register_id::xmm0) == to_byte128(64.125));
-
-  proc->resume();
-  proc->wait_on_signal();
-  REQUIRE(regs.read_by_id_as<long double>(register_id::st0) == 64.125L);
+    proc->resume();
+    proc->wait_on_signal();
+    REQUIRE(regs.read_by_id_as<long double>(register_id::st0) == 64.125L);
 }
