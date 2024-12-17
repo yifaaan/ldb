a79fb4d20b77c5457008c0cf36b49fe49d1a0c07
yifaaan <0219lyf@gmail.com>
2024-12-17 14:17:51 +0800

    Register read

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,6 +7,7 @@
 #include <libldb/process.hpp>
 
 #include "libldb/register_info.hpp"
+#include "libldb/types.hpp"
 
 using namespace ldb;
 
@@ -117,4 +118,30 @@ TEST_CASE("Write register works", "[register]") {
   proc->wait_on_signal();
   output = channel.read();
   REQUIRE(to_string_view(output) == "42.24");
+}
+
+TEST_CASE("Read register works", "[register]") {
+  auto proc =
+      process::launch("/root/workspace/ldb/build/test/targets/reg_read");
+  auto& regs = proc->get_registers();
+  proc->resume();
+  proc->wait_on_signal();
+  REQUIRE(regs.read_by_id_as<std::uint64_t>(register_id::r13) == 0xcafecafe);
+
+  proc->resume();
+  proc->wait_on_signal();
+  REQUIRE(regs.read_by_id_as<std::uint8_t>(register_id::r13b) == 42);
+
+  proc->resume();
+  proc->wait_on_signal();
+  REQUIRE(regs.read_by_id_as<byte64>(register_id::mm0) ==
+          to_byte64(0xba5eba11ull));
+
+  proc->resume();
+  proc->wait_on_signal();
+  REQUIRE(regs.read_by_id_as<byte128>(register_id::xmm0) == to_byte128(64.125));
+
+  proc->resume();
+  proc->wait_on_signal();
+  REQUIRE(regs.read_by_id_as<long double>(register_id::st0) == 64.125L);
 }
