941171d6cf0788f364da85c959cf5f56cac67cea
yifaaan <0219lyf@gmail.com>
2024-12-18 14:09:28 +0800

    Enable breakpoint

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -1,4 +1,7 @@
 #include <libldb/breakpoint_site.hpp>
+#include <libldb/error.hpp>
+#include <libldb/process.hpp>
+#include <sys/ptrace.h>
 
 namespace
 {
@@ -14,4 +17,31 @@ ldb::breakpoint_site::breakpoint_site(process& proc, virt_addr address)
     : process_{&proc}, address_{address}, is_enabled_{false}, saved_data_{}
 {
     id_ = get_next_id();
+}
+
+void ldb::breakpoint_site::enable()
+{
+    if (is_enabled_)
+    {
+        return;
+    }
+
+    errno = 0;
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process_->pid(), address_, nullptr);
+    if (errno != 0)
+    {
+        ldb::error::send_errno("Enabling breakpoint site failed");
+    }
+    // we need only the first 8 bits
+    saved_data_ = static_cast<std::byte>(data & 0xff);
+
+    std::uint64_t int3 = 0xcc;
+    // replace the first 8 bits with int3
+    std::uint64_t data_with_int3 = ((data & ~0xff) | int3);
+
+    if (ptrace(PTRACE_POKEDATA, process_->pid(), address_, data_with_int3) < 0)
+    {
+        ldb::error::send_errno("Enabling breakpoint site failed");
+    }
+    is_enabled_ = true;
 }
