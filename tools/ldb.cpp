4bfd049aa471f2cce83ec8f84e4c146bc72893cd
yifaaan <0219lyf@gmail.com>
2025-01-11 22:38:48 +0800

    Handling User Input

--- tools/ldb.cpp
+++ tools/ldb.cpp
@@ -1,8 +1,14 @@
+#include <algorithm>
 #include <iostream>
+#include <sstream>
 #include <string_view>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <vector>
 
 namespace
 {
@@ -55,6 +61,61 @@ namespace
         }
         return pid;
     }
+
+    std::vector<std::string> Split(std::string_view str, char delimiter)
+    {
+        std::stringstream ss{std::string(str)};
+        std::vector<std::string> out;
+        std::string s;
+        while (std::getline(ss, s, delimiter))
+        {
+            out.push_back(s);
+        }
+        return out;
+    }
+
+    bool IsPrefix(std::string_view str, std::string_view of)
+    {
+        if (str.size() > of.size()) return false;
+        return std::equal(str.begin(), str.end(), of.begin());
+    }
+
+    /// make inferior process continue
+    void Resume(pid_t pid)
+    {
+        if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0)
+        {
+            std::cerr << "Couldn't continue\n";
+            std::exit(1);
+        }
+    }
+
+    void WaitOnSignal(pid_t pid)
+    {
+        int waitStatus;
+        int options = 0;
+        if (waitpid(pid, &waitStatus, options) < 0)
+        {
+            std::perror("waitpid failed");
+            std::exit(1);
+        }
+    }
+
+    void HandleCommand(pid_t pid, std::string_view line)
+    {
+        auto args = Split(line, ' ');
+        auto command = args[0];
+
+        if (IsPrefix(command, "continue"))
+        {
+            Resume(pid);
+            WaitOnSignal(pid);
+        }
+        else
+        {
+            std::cerr << "Unknown command\n";
+        }
+    }
 }
 
 int main(int argc, const char** argv)
@@ -72,5 +133,34 @@ int main(int argc, const char** argv)
     if (waitpid(pid, &waitStatus, options) < 0)
     {
         std::perror("waitpid failed");
+    }
+
+    // user input cmd
+    char* line = nullptr;
+    while ((line = readline("ldb> ")) != nullptr)
+    {
+        std::string lineStr;
+
+        
+        if (lineStr == std::string_view(""))
+        {
+            // empty line: re-run the last command
+            free(line);
+            if (history_length > 0)
+            {
+                lineStr = history_list()[history_length - 1]->line;
+            }
+        }
+        else
+        {
+            lineStr = line;
+            add_history(line);
+            free(line);
+        }
+
+        if (!lineStr.empty())
+        {
+            HandleCommand(pid, line);
+        }
     }
 }
