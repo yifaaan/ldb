8d22ea1d086cbe56eab1e42db8bf04b4ffcd2eb5
yifaaan <0219lyf@gmail.com>
2025-01-14 09:57:55 +0800

    Add commands: Read & Write registers

--- tools/ldb.cpp
+++ tools/ldb.cpp
@@ -1,17 +1,21 @@
-#include "libldb/error.hpp"
 #include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <string_view>
+#include <variant>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
 #include <libldb/process.hpp>
+#include <libldb/error.hpp>
 
 namespace
 {
@@ -95,6 +99,180 @@ namespace
         std::cout << std::endl;
     }
 
+    void PrintHelp(const std::vector<std::string>& args)
+    {
+        if (args.size() == 1)
+        {
+            std::cerr << R"(Available commands:
+continue    - Resume the process
+register    - Commands for operating on registers
+)";
+        }
+        else if (IsPrefix(args[1], "register"))
+        {
+            std::cerr << R"(Available commands:
+read
+read <register>
+read all
+write <register> <value>
+)";
+        }
+        else
+        {
+            std::cerr << "No help available on that\n";
+        }
+    }
+
+
+    /// register read <register>
+    ///
+    /// register read all
+    ///
+    /// register read
+    void HandleRegisterRead(ldb::Process& process, const std::vector<std::string>& args)
+    {
+        auto format = [](auto t)
+        {
+            if constexpr (std::is_floating_point_v<decltype(t)>)
+            {
+                return fmt::format("{}", t);
+            }
+            else if constexpr (std::is_integral_v<decltype(t)>)
+            {
+                return fmt::format("{:#0{}x}", t, sizeof(t) * 2 + 2);
+            }
+            else
+            {
+                return fmt::format("[{:#04x}]", fmt::join(t, ","));
+            }
+        };
+
+        // register read all
+        // register read
+        if (args.size() == 2 or (args.size() == 3 and args[2] == "all"))
+        {
+            for (const auto& info : ldb::GRegisterInfos)
+            {
+                auto shouldPrint = (args.size() == 3 or info.type == ldb::RegisterType::Gpr) and info.name != "orig_rax";
+                if (!shouldPrint) continue;
+                auto value = process.GetRegisters().Read(info);
+                fmt::print("{}:\t{}\n", info.name, std::visit(format, value));
+            }
+        }
+        // register read <register>
+        else if (args.size() == 3)
+        {
+            try
+            {
+                auto info = ldb::RegisterInfoByName(args[2]);
+                auto value = process.GetRegisters().Read(info);
+                fmt::print("{}:\t{}\n", info.name, std::visit(format, value));
+            }
+            catch (ldb::Error& err)
+            {
+                std::cerr << "No such register\n";
+                return;
+            }
+        }
+        else
+        {
+            PrintHelp({"help", "register"});
+        }
+    }
+
+    ldb::Registers::value ParseRegisterValue(ldb::RegisterInfo info, std::string_view text)
+    {
+        try
+        {
+            if (info.format == ldb::RegisterFormat::UInt)
+            {
+                switch (info.size)
+                {
+                case 1: return ldb::ToIntegral<std::uint8_t>(text, 16).value();
+                case 2: return ldb::ToIntegral<std::uint16_t>(text, 16).value();
+                case 4: return ldb::ToIntegral<std::uint32_t>(text, 16).value();
+                case 8: return ldb::ToIntegral<std::uint64_t>(text, 16).value();
+                }
+            }
+            else if (info.format == ldb::RegisterFormat::DoubleFloat)
+            {
+                return ldb::ToFloat<double>(text).value();
+            }
+            else if (info.format == ldb::RegisterFormat::LongDouble)
+            {
+                return ldb::ToFloat<long double>(text).value();
+            }
+            else if (info.format == ldb::RegisterFormat::Vector)
+            {
+                if (info.size == 8)
+                {
+                    return ldb::ParseVector<8>(text);
+                }
+                else if (info.size == 16)
+                {
+                    return ldb::ParseVector<16>(text);
+                }
+            }
+        }
+        catch (...)
+        {
+
+        }
+        ldb::Error::Send("Invalid format");
+
+    }
+
+    /// register write <register name> <value>
+    void HandleRegisterWrite(ldb::Process& process, const std::vector<std::string>& args)
+    {
+        if (args.size() != 4)
+        {
+            PrintHelp({"help", "register"});
+            return;
+        }
+
+        try
+        {
+            auto info = ldb::RegisterInfoByName(args[2]);
+            auto value = ParseRegisterValue(info, args[3]);
+            process.GetRegisters().Write(info, value);
+        }
+        catch (ldb::Error& err)
+        {
+            std::cerr << err.what() << '\n';
+            return;
+        }
+    }
+
+    /// register read
+    ///
+    /// register read all
+    ///
+    /// register read <register name>
+    ///
+    /// register write <register name> <value>
+    void HandleRegisterCommand(ldb::Process& process, const std::vector<std::string>& args)
+    {
+        if (args.size() < 2)
+        {
+            PrintHelp({"help", "register"});
+            return;
+        }
+
+        if (IsPrefix(args[1], "read"))
+        {
+            HandleRegisterRead(process, args);
+        }
+        else if (IsPrefix(args[1], "write"))
+        {
+            HandleRegisterWrite(process, args);
+        }
+        else
+        {
+            PrintHelp({"help", "register"});
+        }
+    }
+
     void HandleCommand(std::unique_ptr<ldb::Process>& process, std::string_view line)
     {
         auto args = Split(line, ' ');
@@ -105,6 +283,14 @@ namespace
             process->Resume();
             auto reason = process->WaitOnSignal();
             PrintStopReason(*process, reason);
+        }
+        else if (IsPrefix(command, "help"))
+        {
+            PrintHelp(args);
+        }
+        else if (IsPrefix(command, "register"))
+        {
+            HandleRegisterCommand(*process, args);
         }
         else
         {
