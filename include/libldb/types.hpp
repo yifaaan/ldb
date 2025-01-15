94dc5e1607c1d6a5dd4b7a429e6ae308f4aedcc4
yifaaan <0219lyf@gmail.com>
2025-01-15 22:12:57 +0800

    Add class Span

--- include/libldb/types.hpp
+++ include/libldb/types.hpp
@@ -2,6 +2,7 @@
 #define LDB_TYPES_HPP
 
 #include <array>
+#include <vector>
 #include <cstddef>
 #include <cstdint>
 
@@ -46,6 +47,25 @@ namespace ldb
 
     private:
         std::uint64_t addr = 0;
+    };
+
+    template<typename T>
+    class Span
+    {
+    public:
+        Span() = default;
+        Span(T* _data, std::size_t _size) : data(_data), size(_size) {}
+        Span(T* _data, T* _end) : data(_data), size(_end - _data) {}
+        template<typename U>
+        Span(const std::vector<U>& vec) : data(vec.data()), size(vec.size()) {}
+
+        T* Begin() const { return data; }
+        T* End() const { return data + size; }
+        std::size_t Size() const { return size; }
+        T& operator[](std::size_t n) { return *(data + n); }
+    private:
+        T* data = nullptr;
+        std::size_t size = 0;
     };
 }
 
