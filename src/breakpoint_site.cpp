3bdd2d471a4c824223464228d624fc677cffb5f4
yifaaan <0219lyf@gmail.com>
2025-01-15 10:11:35 +0800

    Add breakpoint Disable and Enable

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -1,4 +1,9 @@
+#include <cerrno>
+#include <sys/ptrace.h>
+
 #include <libldb/breakpoint_site.hpp>
+#include <libldb/error.hpp>
+#include <libldb/process.hpp>
 
 namespace
 {
@@ -9,6 +14,7 @@ namespace
     }
 }
 
+
 ldb::BreakpointSite::BreakpointSite(Process& proc, VirtAddr _address)
         :process(&proc)
         ,address(_address)
@@ -16,4 +22,47 @@ ldb::BreakpointSite::BreakpointSite(Process& proc, VirtAddr _address)
         ,savedData{}
 {
     id = GetNextId();
+}
+
+void ldb::BreakpointSite::Enable()
+{
+    if (isEnable) return;
+
+    errno = 0;
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process->Pid(), address, nullptr);
+    if (errno != 0)
+    {
+        Error::SendErrno("Enabling breakpoint site failed");
+    }
+
+    savedData = static_cast<std::byte>(data & 0xff);
+
+    std::uint64_t int3 = 0xcc;
+    //  replace the instruction at the given address with an int3 instruction
+    std::uint64_t dataWithInt3 = ((data & ~0xff) | int3);
+
+    if (ptrace(PTRACE_POKEDATA, process->Pid(), address, dataWithInt3) < 0)
+    {
+        Error::SendErrno("Enabling breakpoint site failed");
+    }
+    isEnable = true;
+}
+
+void ldb::BreakpointSite::Disable()
+{
+    if (!isEnable) return;
+
+    errno = 0;
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process->Pid(), address, nullptr);
+    if (errno != 0)
+    {
+        Error::SendErrno("Disabling breakpoint site failed");
+    }
+
+    auto restoredData = (data & ~0xff) | static_cast<std::uint8_t>(savedData);
+    if (ptrace(PTRACE_POKEDATA, process->Pid(), address, restoredData) < 0)
+    {
+        Error::SendErrno("Disabling breakpoint site failed");
+    }
+    isEnable = false;
 }
