d0993838a45cb2efe0788694cbaa6c0b9fec0aaf
yifaaan <0219lyf@gmail.com>
2025-02-28 13:01:55 +0800

    Register interactions

--- include/libldb/register_info.hpp
+++ include/libldb/register_info.hpp
@@ -4,7 +4,9 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <libldb/error.hpp>
 #include <string_view>
+
 namespace ldb {
 enum class RegisterId {
 #define DEFINE_REGISTER(name, dwarf_id, size, offset, type, format) name
@@ -43,4 +45,31 @@ inline constexpr RegisterInfo RegisterInfos[] = {
 #include <libldb/detail/registers.inc>
 #undef DEFINE_REGISTER
 };
+
+template <typename F>
+const RegisterInfo& RegisterInfoBy(F f) {
+  if (auto it =
+          std::find_if(std::begin(RegisterInfos), std::end(RegisterInfos), f);
+      it != std::end(RegisterInfos)) {
+    return *it;
+  }
+  Error::Send("Can't find register info");
+}
+
+// Get register info by id.
+inline const RegisterInfo& RegisterInfoById(RegisterId id) {
+  return RegisterInfoBy([id](const auto& r) { return r.id == id; });
+}
+
+// Get register info by name.
+inline const RegisterInfo& RegisterInfoByName(std::string_view name) {
+  return RegisterInfoBy([name](const auto& r) { return r.name == name; });
+}
+
+// Get register info by dwarf id.
+inline const RegisterInfo& RegisterInfoByDwarfId(std::int32_t dwarf_id) {
+  return RegisterInfoBy(
+      [dwarf_id](const auto& r) { return r.dwarf_id == dwarf_id; });
+}
+
 }  // namespace ldb
