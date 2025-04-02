7efa2dbbe927c1130f72496dcacaf8184cee17e7
Liuyifan <0219lyf@gmail.com>
2025-04-02 19:04:22 +0800

    test launch success

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,23 @@
+#include <sys/types.h>
+#include <signal.h>
+
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("validate environment")
+#include <libldb/process.hpp>
+
+using namespace ldb;
+
+namespace
 {
-	REQUIRE(false);
+	bool ProcessExists(pid_t pid)
+	{
+		auto ret = kill(pid, 0);
+		return ret != -1 && errno != ESRCH;
+	}
+}
+
+TEST_CASE("Process::Launch success", "[process]")
+{
+	auto proc = Process::Launch("yes");
+	REQUIRE(ProcessExists(proc->Pid()));
 }
