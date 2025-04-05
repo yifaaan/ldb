bcd2b00e72d2256cd399d32241b5e5c93287e45f
Liuyifan <0219lyf@gmail.com>
2025-04-05 21:52:58 +0800

    add physical BreakpointSite

---
+++ include/libldb/breakpoint_site.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdint>
+
+#include <libldb/types.hpp>
+
+namespace ldb
+{
+	class Process;
+	class BreakpointSite
+	{
+	public:
+		BreakpointSite() = delete;
+		BreakpointSite(const BreakpointSite&) = delete;
+		BreakpointSite& operator=(const BreakpointSite&) = delete;
+		BreakpointSite(BreakpointSite&&) = delete;
+		BreakpointSite& operator=(BreakpointSite&&) = delete;
+
+		using IdType = std::int32_t;
+		IdType Id() const { return id; }
+
+		void Enable();
+		void Disable();
+
+		bool IsEnabled() const { return isEnabled; }
+
+		VirtAddr Address() const { return address; }
+
+		bool AtAddress(VirtAddr addr) const { return address == addr; }
+
+		bool InRange(VirtAddr low, VirtAddr high) const { return low <= address && address < high; }
+
+
+	private:
+		BreakpointSite(Process& proc, VirtAddr addr);
+		friend Process;
+
+		IdType id;
+		Process* process;
+		VirtAddr address;
+		bool isEnabled;
+		std::_Fnv1a_append_bytes savedData;
+	};
+}
