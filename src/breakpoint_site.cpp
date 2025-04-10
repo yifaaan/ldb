324c30fa5bf060b3c938f5d58037f1f21a780e2a
Liuyifan <0219lyf@gmail.com>
2025-04-10 16:29:24 +0800

    enable breakpoint site

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -1,4 +1,7 @@
+#include <sys/ptrace.h>
 
+#include <libldb/process.hpp>
+#include <libldb/error.hpp>
 #include <libldb/breakpoint_site.hpp>
 
 namespace
@@ -16,5 +19,25 @@ namespace ldb
 			: process(&proc), address(addr), isEnabled(false)
 	{
 		id = GetNextId();
+	}
+
+	void BreakpointSite::Enable()
+	{
+		if (isEnabled) return;
+
+		errno = 0;
+		std::uint64_t data = ptrace(PTRACE_PEEKDATA, process->Pid(), address, nullptr);
+		if (errno != 0)
+		{
+			Error::SendErrno("Enabling breakpoint site failed");
+		}
+		savedData = static_cast<std::byte>(data & 0xff);
+		std::uint64_t int3 = 0xcc;
+		std::uint64_t dataWithInt3 = (data & ~0xff) | int3;
+		if (ptrace(PTRACE_POKEDATA, process->Pid(), address, dataWithInt3) < 0)
+		{
+			Error::SendErrno("Enabling breakpoint site failed");
+		}
+		isEnabled = true;
 	}
 }
