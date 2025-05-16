ea31ab62807ae9f858ef472e04e8f5bfa51e058b
yifaaan <0219lyf@gmail.com>
2025-05-16 23:03:53 +0800

    enable and disable breakpoint site

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -1,4 +1,7 @@
+#include <sys/ptrace.h>
+#include <cerrno>
 #include <libldb/breakpoint_site.hpp>
+#include <libldb/process.hpp>
 
 namespace
 {
@@ -16,4 +19,46 @@ ldb::breakpoint_site::breakpoint_site(process& proc, virt_addr address)
     , saved_data_{}
 {
     id_ = get_next_id();
+}
+
+void ldb::breakpoint_site::enable()
+{
+    if (is_enabled_) return;
+
+    errno = 0;
+    // 读取地址处的数据
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process_->pid(), addr_, nullptr);
+    if (errno != 0)
+    {
+        error::send_errno("Enable breakpoint site failed");
+    }
+    // 保存地址处的数据
+    saved_data_ = static_cast<std::byte>(data & 0xff);
+    // 设置断点
+    std::uint64_t int3 = 0xcc;
+    // 将地址处的数据设置为int3
+    std::uint64_t data_with_int3 = (data & ~0xff) | int3;
+    // 写入地址处
+    if (ptrace(PTRACE_POKEDATA, process_->pid(), addr_, data_with_int3) < 0)
+    {
+        error::send_errno("Enable breakpoint site failed");
+    }
+    is_enabled_ = true;
+}
+
+void ldb::breakpoint_site::disable()
+{
+    if (!is_enabled_) return;
+    errno = 0;
+    std::uint64_t data = ptrace(PTRACE_PEEKDATA, process_->pid(), addr_, nullptr);
+    if (errno != 0)
+    {
+        error::send_errno("Disable breakpoint site failed");
+    }
+    auto restored_data = (data & ~0xff) | static_cast<std::uint8_t>(saved_data_);
+    if (ptrace(PTRACE_POKEDATA, process_->pid(), addr_, restored_data) < 0)
+    {
+        error::send_errno("Disable breakpoint site failed");
+    }
+    is_enabled_ = false;
 }
