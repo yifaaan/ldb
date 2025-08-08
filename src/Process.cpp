1fec8b94839ea87aa0547b2af50fcb8a812c3664
yifaaan <0219lyf@gmail.com>
2025-08-08 13:23:26 +0800

    Error Handler

--- src/Process.cpp
+++ src/Process.cpp
@@ -1,5 +1,7 @@
 #include <libldb/Process.h>
 
+#include <libldb/Error.h>
+
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -10,17 +12,19 @@ namespace ldb
     std::unique_ptr<Process> Process::Launch(std::filesystem::path path)
     {
         pid_t pid;
-        if ((pid = fork()) < 0) {
-            // TODO: Handle error
+        if ((pid = fork()) < 0)
+        {
+            Error::SendErrno("fork failed");
         }
-        if (pid == 0) {
+        if (pid == 0)
+        {
             if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0)
             {
-                // TODO: Handle error
+                Error::SendErrno("Tracing failed");
             }
             if (execlp(path.c_str(), path.c_str(), nullptr) < 0)
             {
-                // TODO: Handle error
+                Error::SendErrno("exec failed");
             }
         }
 
@@ -31,13 +35,17 @@ namespace ldb
 
     std::unique_ptr<Process> Process::Attach(pid_t pid)
     {
+        if (pid == 0)
+        {
+            Error::Send("Invalid PID");
+        }
         if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0)
         {
-            // TODO: Handle error
+            Error::SendErrno("Could not attach");
         }
 
         std::unique_ptr<Process> process(new Process(pid, false));
         process->WaitOnSignal();
         return process;
     }
-}
+} // namespace ldb
